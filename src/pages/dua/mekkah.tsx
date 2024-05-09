baf1efcaf16944cfbff4130d7376ef02f404679d
Itachi-MacOS <warrior.006@inbox.ru>
2024-05-09 06:42:35 +0500

    mekkah

--- src/pages/dua/mekkah.tsx
+++ src/pages/dua/mekkah.tsx
@@ -1,8 +1,18 @@
 import { useNavigate } from 'react-router-dom'
 import ModuleHeader from '../../modules/header/module-header'
 import UICell from '../../ui/cell/ui-cell'
+import { useSystem } from '../../store/system'
+import {
+  al_haram_enter,
+  al_haram_exit,
+  bye_kaahbah_muaz,
+  mekkah_enter,
+  see_kaabah_muaz,
+  zhannat_salem
+} from '../../audios/mekkah'
 
 const DuaMekkahPage = () => {
+  const { setCurrentPrayer, showPlayerModal } = useSystem()
   const navigate = useNavigate()
   return (
     <>
@@ -14,14 +24,69 @@ const DuaMekkahPage = () => {
         }}
       />
       <div className="px-4 space-y-4 pt-2 pb-[80px]">
-        <UICell level={99}>Мекке Мүкәррамаға кірерде оқылатын дұға</UICell>
-        <UICell level={9}>әл-Харамға кірерде оқылатын дұға</UICell>
-        <UICell level={8}>Қағба муаззаманы көргенде оқылатын дұға</UICell>
-        <UICell level={7}>әл-Харамнан шығарда оқылатын дұға</UICell>
-        <UICell level={6}>Мүлтазамда оқылатын дұға</UICell>
+        <UICell
+          onClick={() => {
+            setCurrentPrayer(mekkah_enter)
+            showPlayerModal()
+          }}
+          level={99}
+        >
+          Мекке Мүкәррамаға кірерде оқылатын дұға
+        </UICell>
+        <UICell
+          onClick={() => {
+            setCurrentPrayer(al_haram_enter)
+            showPlayerModal()
+          }}
+          level={9}
+        >
+          әл-Харамға кірерде оқылатын дұға
+        </UICell>
+        <UICell
+          onClick={() => {
+            setCurrentPrayer(see_kaabah_muaz)
+            showPlayerModal()
+          }}
+          level={8}
+        >
+          Қағба муаззаманы көргенде оқылатын дұға
+        </UICell>
+        <UICell
+          onClick={() => {
+            setCurrentPrayer(al_haram_exit)
+            showPlayerModal()
+          }}
+          level={7}
+        >
+          әл-Харамнан шығарда оқылатын дұға
+        </UICell>
+        <UICell
+          onClick={() => {
+            //
+          }}
+          level={6}
+        >
+          Мүлтазамда оқылатын дұға
+        </UICell>
         <UICell level={5}>Хадиша анамызға сәлем беру</UICell>
-        <UICell level={5}>Жәннәт ул-мұғаллаға сәлем беру</UICell>
-        <UICell level={5}>ҚАҒБА МУАЗЗАМАМЕН ҚОШТАСАРДА ОҚЫЛАТЫН ДҰҒА</UICell>
+        <UICell
+          onClick={() => {
+            setCurrentPrayer(zhannat_salem)
+            showPlayerModal()
+          }}
+          level={5}
+        >
+          Жәннәт ул-мұғаллаға сәлем беру
+        </UICell>
+        <UICell
+          onClick={() => {
+            setCurrentPrayer(bye_kaahbah_muaz)
+            showPlayerModal()
+          }}
+          level={5}
+        >
+          ҚАҒБА МУАЗЗАМАМЕН ҚОШТАСАРДА ОҚЫЛАТЫН ДҰҒА
+        </UICell>
       </div>
     </>
   )
