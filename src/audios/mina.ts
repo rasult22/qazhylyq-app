0067ab4f743e85c2f6e299de13f3670792586bcb
Itachi-MacOS <warrior.006@inbox.ru>
2024-05-09 07:28:37 +0500

    mina prayers

---
+++ src/audios/mina.ts
@@ -0,0 +1,34 @@
+import mina_arrival_audio from '../../assets/mina_arrival.mp3'
+import mina_after_muzdalifa_audio from '../../assets/mina_after_muzdalifa.mp3'
+import mina_to_arafat_audio from '../../assets/mina_after_muzdalifa.mp3'
+
+export const mina_arrival = {
+  audio_url: mina_arrival_audio,
+  title: 'Отсутсвует',
+  title_ru: 'Отсутсвует',
+  title_tt: `Отсутсвует`,
+  prayer_text: `Отсутсвует`,
+  translation_text: `Отсутсвует`,
+  translation_text_ru: `Отсутсвует`,
+  translation_text_tt: `Отсутсвует`
+}
+export const mina_after_muzdalifa = {
+  audio_url: mina_after_muzdalifa_audio,
+  title: 'Отсутсвует',
+  title_ru: 'Отсутсвует',
+  title_tt: `Отсутсвует`,
+  prayer_text: `Отсутсвует`,
+  translation_text: `Отсутсвует`,
+  translation_text_ru: `Отсутсвует`,
+  translation_text_tt: `Отсутсвует`
+}
+export const mina_to_arafat = {
+  audio_url: mina_to_arafat_audio,
+  title: 'Отсутсвует',
+  title_ru: 'Отсутсвует',
+  title_tt: `Отсутсвует`,
+  prayer_text: `Отсутсвует`,
+  translation_text: `Отсутсвует`,
+  translation_text_ru: `Отсутсвует`,
+  translation_text_tt: `Отсутсвует`
+}
--- src/pages/dua/mina.tsx
+++ src/pages/dua/mina.tsx
@@ -1,9 +1,16 @@
 import { useNavigate } from 'react-router-dom'
 import UICell from '../../ui/cell/ui-cell'
 import ModuleHeader from '../../modules/header/module-header'
+import { useSystem } from '../../store/system'
+import {
+  mina_after_muzdalifa,
+  mina_arrival,
+  mina_to_arafat
+} from '../../audios/mina'
 
 const DuaMinaPage = () => {
   const navigate = useNavigate()
+  const { showPlayerModal, setCurrentPrayer } = useSystem()
   return (
     <>
       <ModuleHeader
@@ -14,9 +21,33 @@ const DuaMinaPage = () => {
         }}
       />
       <div className="px-4 space-y-4 pt-2 pb-[80px]">
-        <UICell level={99}>Минаға жеткенде оқылатын дұға</UICell>
-        <UICell level={9}>Минадан Арафатқа шығар кезде оқылатын дұға</UICell>
-        <UICell level={8}>Мұздалифадан кейін Минада оқылатын дұға</UICell>
+        <UICell
+          onClick={() => {
+            setCurrentPrayer(mina_arrival)
+            showPlayerModal()
+          }}
+          level={99}
+        >
+          Минаға жеткенде оқылатын дұға
+        </UICell>
+        <UICell
+          onClick={() => {
+            setCurrentPrayer(mina_to_arafat)
+            showPlayerModal()
+          }}
+          level={9}
+        >
+          Минадан Арафатқа шығар кезде оқылатын дұға
+        </UICell>
+        <UICell
+          onClick={() => {
+            setCurrentPrayer(mina_after_muzdalifa)
+            showPlayerModal()
+          }}
+          level={8}
+        >
+          Мұздалифадан кейін Минада оқылатын дұға
+        </UICell>
       </div>
     </>
   )
