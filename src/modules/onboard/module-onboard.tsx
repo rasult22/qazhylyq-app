28a39c3408a742b198912844ce9c5d041740c8c8
Itachi-MacOS <warrior.006@inbox.ru>
2024-07-11 08:24:49 +0500

    adding animations

--- src/modules/onboard/module-onboard.tsx
+++ src/modules/onboard/module-onboard.tsx
@@ -1,23 +1,36 @@
 import React, { useEffect, useState } from 'react'
 import logo from './logo.png'
+import { motion, AnimatePresence } from 'framer-motion'
+
 const ModuleOnboard: React.FC = () => {
   const [isOpen, setIsOpen] = useState(true)
   useEffect(() => {
     setTimeout(() => {
       setIsOpen(false)
-    }, 1000)
+    }, 2000)
   }, [])
   return (
-    <div
-      className={`${
-        isOpen ? 'block' : 'hidden'
-      } fixed top-0 w-full h-full bg-white flex flex-col items-center justify-center`}
-    >
-      <img className="w-[248px] mb-[40px]" src={logo} alt="Module Onboard" />
-      <h1 className="text-[24px] leading-[28px] text-[#B19576] uppercase font-bold mb-4">
-        Қажылық және <br /> ұмра дұғалары
-      </h1>
-    </div>
+    <>
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            initial={{ opacity: 0, scale: 0.9 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 1.4 }}
+            className={`fixed top-0 w-full h-full bg-white flex flex-col items-center justify-center`}
+          >
+            <img
+              className="w-[248px] mb-[40px]"
+              src={logo}
+              alt="Module Onboard"
+            />
+            <h1 className="text-[24px] leading-[28px] text-[#B19576] uppercase font-bold mb-4">
+              Қажылық және <br /> ұмра дұғалары
+            </h1>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </>
   )
 }
 
